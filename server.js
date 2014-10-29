c96cff30d04f2661b247a27b56bb8307438d6282
Dmitry Panasenko <d.panasenko@fpmsoft.com>
2014-10-29 09:39:01 +0200

    init

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+var ver = 1.0;
+console.log("started" + ver);
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hi v:" + ver);
+  response.end();
+}).listen(8888);
