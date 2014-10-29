ce57fdac96db5f0c7a8ed3f4a807c7484bd461ec
Dmitry Panasenko <d.panasenko@fpmsoft.com>
2014-10-29 10:34:07 +0200

    1.4

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require("http");
-var ver = 1.3;
+var ver = 1.4;
 console.log("started" + ver);
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
